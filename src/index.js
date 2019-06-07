e7bad55e1193411d80e0e36b03300786a2cf4543
Eva_Sh <shmargunovaevgeniya@yandex.ru>
2019-06-07 13:09:53 +0300

    create approval card

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom"; 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetails';
+import ApprovalCard from './ApprovalCard';
+import faker from "faker";
 
 const App = () => {
-  return <div>Hi there!</div> ;
+    return (
+        <div className='ui container comments'>
+            <ApprovalCard>
+                <CommentDetail
+                    author='Jim'
+                    timeAgo='13:23'
+                    content='Time for what?'
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author='Mary'
+                    timeAgo='13:24'
+                    content='Revolution'
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author='Lulu'
+                    timeAgo='13:25'
+                    content='Oppps'
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
